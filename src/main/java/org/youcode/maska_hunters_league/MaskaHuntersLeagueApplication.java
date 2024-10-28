275189faa401cb944a87336b17ebf273bbe555c7
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-10-28 22:24:03 +0100

    first commit

---
+++ src/main/java/org/youcode/maska_hunters_league/MaskaHuntersLeagueApplication.java
@@ -0,0 +1,13 @@
+package org.youcode.maska_hunters_league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskaHuntersLeagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaskaHuntersLeagueApplication.class, args);
+    }
+
+}
