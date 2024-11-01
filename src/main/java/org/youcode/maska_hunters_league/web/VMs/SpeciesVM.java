35c8911cb413ca00488cf7fa86504ce034811105
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-01 12:20:27 +0100

    add costume annotation for enums

--- src/main/java/org/youcode/maska_hunters_league/web/VMs/SpeciesVM.java
+++ src/main/java/org/youcode/maska_hunters_league/web/VMs/SpeciesVM.java
@@ -1,18 +1,36 @@
 package org.youcode.maska_hunters_league.web.VMs;
 
+import jakarta.validation.constraints.*;
+import lombok.*;
 import org.youcode.maska_hunters_league.domain.enums.Difficulty;
 import org.youcode.maska_hunters_league.domain.enums.SpeciesType;
+import org.youcode.maska_hunters_league.validation.EnumValue;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SpeciesVM {
 
+    @NotBlank
+    @NotNull
     private String name;
 
-    private SpeciesType category;
+    @NotBlank
+    @EnumValue(enumClass = SpeciesType.class, message = "Invalid category")
+    private String category;
 
+    @NotNull
+    @Positive
     private Double minimumWeight;
 
-    private Difficulty difficulty;
+    @NotBlank
+    @EnumValue(enumClass = Difficulty.class, message = "Invalid difficulty")
+    private String difficulty;
 
+    @NotNull
+    @Min(value = 0, message = "Points must be at least 0")
     private Integer points;
 
 }
