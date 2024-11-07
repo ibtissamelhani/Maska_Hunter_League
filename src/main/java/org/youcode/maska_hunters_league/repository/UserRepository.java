2c3161a8074550c8700c9830e3c09603d332c14c
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-07 20:54:40 +0100

    add search method

--- src/main/java/org/youcode/maska_hunters_league/repository/UserRepository.java
+++ src/main/java/org/youcode/maska_hunters_league/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package org.youcode.maska_hunters_league.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.youcode.maska_hunters_league.domain.entities.User;
 
 import java.util.List;
@@ -11,4 +13,8 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     List<User> findByUsernameContainingOrEmailContaining(String name, String email);
+
+    @Query("SELECT u FROM User u WHERE LOWER(u.username) LIKE LOWER(CONCAT('%', :term, '%')) " +
+            "OR LOWER(u.email) LIKE LOWER(CONCAT('%', :term, '%'))")
+    List<User> searchByUsernameOrEmail(@Param("term") String term);
 }
--- src/main/java/org/youcode/maska_hunters_league/service/CompetitionService.java
+++
@@ -1,4 +0,0 @@
-package org.youcode.maska_hunters_league.service;
-
-public interface CompetitionService {
-}
