ad39c74b60c1d25cdb40ed952ee78f8bf4c5831d
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-08 17:26:06 +0100

    CreateCompetitionVM & its mapper added

---
+++ src/main/java/org/youcode/maska_hunters_league/web/VMs/CreateCompetitionVM.java
@@ -0,0 +1,40 @@
+package org.youcode.maska_hunters_league.web.VMs;
+
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.youcode.maska_hunters_league.domain.enums.SpeciesType;
+import org.youcode.maska_hunters_league.validation.EnumValue;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateCompetitionVM {
+
+    @NotBlank
+    private String location;
+
+    @NotNull
+    @Future
+    private LocalDateTime date;
+
+    @NotNull
+    @EnumValue(enumClass = SpeciesType.class , message = "invalid species Type")
+    private String speciesType;
+
+    @Min(1)
+    private Integer minParticipants;
+
+    @NotNull
+    @Min(1)
+    private Integer maxParticipants;
+
+    private Boolean openRegistration = true;
+}
