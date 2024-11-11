d2e0afbd210e23770af7e8dda81fa5bae2301d35
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-11 09:57:36 +0100

    add competition vm

--- src/main/java/org/youcode/maska_hunters_league/web/rest/CompetitionController.java
+++ src/main/java/org/youcode/maska_hunters_league/web/rest/CompetitionController.java
@@ -3,13 +3,16 @@ package org.youcode.maska_hunters_league.web.rest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.youcode.maska_hunters_league.domain.entities.Competition;
 import org.youcode.maska_hunters_league.service.CompetitionService;
+import org.youcode.maska_hunters_league.web.VMs.CompetitionVM;
 import org.youcode.maska_hunters_league.web.VMs.CreateCompetitionVM;
+import org.youcode.maska_hunters_league.web.VMs.mapper.CompetitionVMMapper;
 import org.youcode.maska_hunters_league.web.VMs.mapper.CreateCompetitionVMMapper;
 
 import java.util.List;
@@ -23,11 +26,14 @@ public class CompetitionController {
 
     private final CompetitionService competitionService;
     private final CreateCompetitionVMMapper createCompetitionVMMapper;
+    private final CompetitionVMMapper competitionVMMapper;
 
     @GetMapping
-    public ResponseEntity<Page<Competition>> getAllCompetitions(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+    public ResponseEntity<Page<CompetitionVM>> getAllCompetitions(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
         Page<Competition> competitions = competitionService.findAllCompetitionsPaginated(page,size);
-        return ResponseEntity.ok(competitions);
+        List<CompetitionVM> competitionVMSList = competitions.stream().map(competitionVMMapper::toCompetitionVM).toList();
+        Page<CompetitionVM> competitionVMS = new PageImpl<>(competitionVMSList,competitions.getPageable(),competitions.getTotalElements());
+        return ResponseEntity.ok(competitionVMS);
     }
 
     @PostMapping()
