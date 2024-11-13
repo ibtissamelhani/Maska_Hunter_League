81035b825ec5812b71b218758d2558b157a9beec
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-13 21:00:34 +0100

    adding AuditorAwareImpl

--- src/main/java/org/youcode/maska_hunters_league/MaskaHuntersLeagueApplication.java
+++ src/main/java/org/youcode/maska_hunters_league/MaskaHuntersLeagueApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class MaskaHuntersLeagueApplication {
 
     public static void main(String[] args) {
