b0f6cf365bcdb45b809ceb79df066aec5df76236
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-11-13 20:47:29 +0100

    Enable Jpa Auditing

--- src/main/java/org/youcode/maska_hunters_league/MaskaHuntersLeagueApplication.java
+++ src/main/java/org/youcode/maska_hunters_league/MaskaHuntersLeagueApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing
 public class MaskaHuntersLeagueApplication {
 
     public static void main(String[] args) {
