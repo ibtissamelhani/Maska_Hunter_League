0fbbb4fccbc1321a3a2fe538e5d9f1c621d08dec
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-12-11 10:03:31 +0100

    SecurityConfiguration updated

--- src/main/java/org/youcode/maska_hunters_league/config/SecurityConfiguration.java
+++ src/main/java/org/youcode/maska_hunters_league/config/SecurityConfiguration.java
@@ -8,12 +8,14 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.youcode.maska_hunters_league.security.JwtAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import static org.youcode.maska_hunters_league.domain.enums.Permission.*;
 import static org.youcode.maska_hunters_league.domain.enums.Role.*;
 
@@ -30,32 +32,20 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**")
-                    .permitAll()
-//                .requestMatchers("/api/v1/species/**").hasRole(ADMIN.name())
-/*
+                .csrf(AbstractHttpConfigurer::disable)
 
-                .requestMatchers(GET, "/api/v1/competition/**").hasAuthority(CAN_VIEW_COMPETITIONS.name())
-                .requestMatchers(POST, "/api/v1/competition/**").hasAuthority(CAN_MANAGE_COMPETITIONS.name())
-                .requestMatchers(PUT, "/api/v1/competition/**").hasAuthority(CAN_MANAGE_COMPETITIONS.name())
-                .requestMatchers(DELETE, "/api/v1/competition/**").hasAuthority(CAN_MANAGE_COMPETITIONS.name())
-*/
-
-                .requestMatchers("/api/v1/hunt").hasAnyRole(ADMIN.name(), JURY.name())
-
-                .requestMatchers(POST,"/api/v1/participation/**").hasAuthority(CAN_PARTICIPATE.name())
-                .requestMatchers(GET,"/api/v1/participation/**").hasAuthority(CAN_VIEW_RANKINGS.name())
-
-                .requestMatchers("/api/v1/users/**").hasRole(ADMIN.name())
-                .anyRequest()
-                    .authenticated()
-                .and()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .authorizeHttpRequests(
+                        authorizeHttpRequest -> {
+                            authorizeHttpRequest.requestMatchers("/api/v1/auth/**")
+                                    .permitAll();
+                            authorizeHttpRequest.requestMatchers("/api/v1/hunt").hasAnyRole(ADMIN.name(), JURY.name());
+                            authorizeHttpRequest.requestMatchers(POST,"/api/v1/participation/**").hasAuthority(CAN_PARTICIPATE.name());
+                            authorizeHttpRequest.requestMatchers(GET,"/api/v1/participation/**").hasAuthority(CAN_VIEW_RANKINGS.name());
+                            authorizeHttpRequest.requestMatchers(GET,"/api/v1/participation/**").hasAuthority(CAN_VIEW_RANKINGS.name());
+                            authorizeHttpRequest.requestMatchers("/api/v1/users/**").hasRole(ADMIN.name());
+                            authorizeHttpRequest.anyRequest().authenticated();
+                        }
+                ).sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
