268fe4a83d5efe1c6a35af9ae0c5a6dd7d2bfbc1
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-12-28 14:11:58 +0100

    web config added

--- src/main/java/org/youcode/maska_hunters_league/web/rest/AuthenticationController.java
+++ src/main/java/org/youcode/maska_hunters_league/web/rest/AuthenticationController.java
@@ -5,16 +5,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.youcode.maska_hunters_league.service.AuthService;
 import org.youcode.maska_hunters_league.service.Implementations.AuthenticationService;
 import org.youcode.maska_hunters_league.web.VMs.AuthVMs.AuthenticationRequest;
 import org.youcode.maska_hunters_league.web.VMs.AuthVMs.AuthenticationResponse;
 import org.youcode.maska_hunters_league.web.VMs.AuthVMs.RegisterRequest;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
