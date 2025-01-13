0609416f49f006d60bc8f58209daf3bd6bf4b235
ibtissam el hani <ibtissamelhani36@gmail.com>
2025-01-13 15:40:58 +0100

    soft delete species

--- src/main/java/org/youcode/maska_hunters_league/domain/entities/Species.java
+++ src/main/java/org/youcode/maska_hunters_league/domain/entities/Species.java
@@ -2,9 +2,12 @@ package org.youcode.maska_hunters_league.domain.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.youcode.maska_hunters_league.domain.enums.Difficulty;
 import org.youcode.maska_hunters_league.domain.enums.SpeciesType;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
@@ -13,6 +16,8 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLDelete(sql = "UPDATE species SET deleted = true, deleted_at = NOW() WHERE id=?")
+@Where(clause = "deleted=false")
 public class Species extends Auditable{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -30,5 +35,11 @@ public class Species extends Auditable{
     private Difficulty difficulty;
 
     private Integer points;
+
+    @Column(name = "deleted")
+    private boolean deleted = false;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
 }
